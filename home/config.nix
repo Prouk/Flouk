a00c538623a87a45e27041e2fbb2a7a59dbc5d64
Prouk <valentin.tahon2@gmail.com>
2025-05-05 23:44:09 +0200

    Update config.nix

--- home/config.nix
+++ home/config.nix
@@ -1,6 +1,5 @@
 { ... }:
 {
-  home.file.".config/ags".source = config/ags;
   home.file.".config/fastfetch".source = config/fastfetch;
   home.file.".config/hypr".source = config/hypr;
   home.file.".config/kitty".source = config/kitty;
