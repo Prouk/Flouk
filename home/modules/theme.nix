1c056a223de0633a135028c852517043abe5a2d8
Prouk <valentin.tahon2@gmail.com>
2025-05-06 00:48:21 +0200

    dark themed

--- home/modules/theme.nix
+++ home/modules/theme.nix
@@ -2,15 +2,24 @@
 {
   home.pointerCursor = {
     enable = true;
-    gtk.enable = true;
-    x11.enable = true;
     package = pkgs.google-cursor;
     name = "GoogleDot-White";
     size = 24;
-    hyprcursor.size = 24;
   };
-  gtk.theme = {
-    package = pkgs.graphite-gtk-theme;
-    name = "Graphite-Nord-Black";
+
+  gtk = {
+    enable = true;
+    theme = {
+        package = pkgs.graphite-gtk-theme;
+        name = "Graphite-Dark";
+
+    };
+    cursorTheme = {
+      name = "GoogleDot-White";
+    };
+    iconTheme = {
+      package = pkgs.nordzy-icon-theme;
+      name = "Nordzy-Dark";
+    };
   };
 }
