88b1b9394a0a83434785bd106547056408df1775
Prouk <valentin.tahon2@gmail.com>
2025-06-16 17:52:24 +0200

    removed stolen prop

--- home/config.nix
+++ home/config.nix
@@ -1,6 +1,7 @@
 { ... }:
 {
   home.file.".config/fastfetch".source = config/fastfetch;
+  home.file.".config/ags".source = config/ags;
   home.file.".config/helix".source = config/helix;
   home.file.".config/hypr".source = config/hypr;
   home.file.".config/kitty".source = config/kitty;
