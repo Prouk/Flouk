6a7d92693db50652cbc5fc46a3ab24660feed141
Prouk <valentin.tahon2@gmail.com>
2025-06-19 17:43:58 +0200

    direnv conf through h-m

--- home/config.nix
+++ home/config.nix
@@ -2,7 +2,6 @@
 {
   home.file.".config/fastfetch".source = config/fastfetch;
   home.file.".config/helix".source = config/helix;
-  home.file.".config/direnv".source = config/direnv;
   home.file.".config/hypr".source = config/hypr;
   home.file.".config/kitty".source = config/kitty;
   home.file.".config/rofi".source = config/rofi;
